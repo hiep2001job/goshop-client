ac4500678c4c82f1edd7d3b132ada274c339e07a
Hiep Nguyen Thanh <hiep2001.job@gmail.com>
2023-05-06 21:57:36 +0700

    Added Product detail page

--- src/app/layout/App.tsx
+++ src/app/layout/App.tsx
@@ -1,10 +1,16 @@
 import React, { useEffect, useState } from 'react';
 
 import './style.css';
-import { Product } from '../models/product';
+
 import Catalog from '../../features/catalog/Catalog';
 import Header from './Header';
 import { Container, CssBaseline, ThemeProvider, createTheme } from '@mui/material';
+import { Route, Routes } from 'react-router-dom';
+import Home from '../../features/home/Home';
+import ProductDetail from '../../features/catalog/ProductDetail';
+import About from '../../features/about/About';
+import Contact from '../../features/contact/Contact';
+
 
 function App() {
   const [darkMode, setDarkMode] = useState(true);
@@ -27,7 +33,13 @@ function App() {
       <CssBaseline />
       <Header darkMode={darkMode} handleThemeChange={handleThemeChange} />
       <Container>
-        <Catalog />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/catalog' element={<Catalog />} />
+          <Route path='/catalog/:id' element={<ProductDetail />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/contact' element={<Contact />} />
+        </Routes>
       </Container>
     </ThemeProvider>
 
--- src/features/catalog/ProductCard.tsx
+++ src/features/catalog/ProductCard.tsx
@@ -1,5 +1,6 @@
 import { Avatar, Button, Card, CardActions, CardContent, CardHeader, CardMedia, Typography } from '@mui/material'
 import { Product } from '../../app/models/product'
+import { Link } from 'react-router-dom';
 
 interface Props {
     product: Product;
@@ -22,8 +23,7 @@ const ProductCard = ({ product }: Props) => {
 
             <CardMedia
                 sx={{ height: 140, backgroundSize: 'contain',bgcolor:'primary.light'}}
-                // image={product.pictureUrl}
-                image='http://picsum.photos/300'
+                image={product.pictureUrl}                
                 title={product.name}
             />
             <CardContent>
@@ -36,7 +36,7 @@ const ProductCard = ({ product }: Props) => {
             </CardContent>
             <CardActions>
                 <Button size="small">Add To Cart</Button>
-                <Button size="small">View</Button>
+                <Button component={Link} to={`/catalog/${product.id}`} size="small">View</Button>
             </CardActions>
         </Card>
     )
